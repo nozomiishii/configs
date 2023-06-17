39091cf3cd886b2c43a564a9eecbcd610e91972b
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-06-18 02:22:50 +0900

    chore(cspell): change cli script from node to bash

---
+++ packages/cspell-config/bin/cli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# -C          : Prevent overwriting files with output redirection
+# -e          : Exit the script if any command returns a non-zero status
+# -u          : Exit the script if an undefined variable is used
+# -o pipefail : Change pipeline exit status to the last non-zero exit
+#               code in the pipeline, or zero if all commands succeed
+# -x          : (Optional) Enable command tracing for easier debugging
+set -Ceuo pipefail
+
+configPath="$(realpath "$(dirname "$0")/../index.cjs")"
+
+npx -y cspell --config "$configPath" "$@"
