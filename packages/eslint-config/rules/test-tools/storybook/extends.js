4d65d4d0a967028ff8e0172c93d58a0e85dadeae
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-07-02 01:37:00 +0900

    chore(eslint): set 'import/no-extraneous-dependencies': 'off'

--- packages/eslint-config/rules/test-tools/storybook/extends.js
+++ packages/eslint-config/rules/test-tools/storybook/extends.js
@@ -4,7 +4,14 @@ const { defineConfig } = require('eslint-define-config');
 module.exports = defineConfig({
   overrides: [
     {
-      files: ['**/*.stories.*', '**/.storybook/main.*'],
+      files: ['**/.storybook/**', '**/.storybook**'],
+      rules: {
+        // devDependenciesで良い
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+    {
+      files: ['**/*.stories.*'],
       extends: [
         /**
          * eslint-plugin-storybook
@@ -19,6 +26,9 @@ module.exports = defineConfig({
          * {@link https://github.com/storybookjs/eslint-plugin-storybook/blob/main/docs/rules/csf-component.md}
          */
         'storybook/csf-component': 'error',
+
+        // testing LibraryのuserEventの戻り値の型がpromiseではないがawaitで書かないと予期した挙動にならない
+        '@typescript-eslint/await-thenable': 'off',
       },
     },
   ],
