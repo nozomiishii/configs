2828117d8f01fb56a56cbd6489843c0afb658463
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-07-04 00:07:51 +0900

    chore(eslint): add ignore pattern for .storybook directory

--- packages/eslint-config/rules/test-tools/storybook/extends.js
+++ packages/eslint-config/rules/test-tools/storybook/extends.js
@@ -2,6 +2,7 @@
 const { defineConfig } = require('eslint-define-config');
 
 module.exports = defineConfig({
+  ignorePatterns: ['!**/.storybook'],
   overrides: [
     {
       files: ['**/.storybook/**', '**/.storybook**'],
