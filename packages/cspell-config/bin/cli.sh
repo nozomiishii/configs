43fb4b39ee6748e44f10b2273b436fa6aa92c937
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-12-15 19:50:34 +0900

    feat: change runtime from node to bash

---
+++ packages/cspell-config/bin/cli.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# -C          : Prevent overwriting files with output redirection
+# -e          : Exit the script if any command returns a non-zero status
+# -u          : Exit the script if an undefined variable is used
+# -o pipefail : Change pipeline exit status to the last non-zero exit
+#               code in the pipeline, or zero if all commands succeed
+# -x          : (Optional) Enable command tracing for easier debugging
+set -Ceuo pipefail
+
+configPath=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../index.cjs")
+
+args=("${@:1}")
+
+npx -y cspell --config "$configPath" "${args[@]}"
+
+exit $?
