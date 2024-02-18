1751de2e367b935821d8645a535eeda562c5e1bc
Nozomi Ishii <nozomiishii.jp@gmail.com>
2024-02-18 13:38:42 +0900

    feat: create @nozomiishii/commitlint-config

---
+++ packages/commitlint-config/index.js
@@ -0,0 +1,12 @@
+// @ts-check
+/**
+ * Configuration
+ * {@link https://commitlint.js.org/#/reference-configuration}
+ *
+ * Rules
+ * {@link https://commitlint.js.org/#/reference-rules}
+ *
+ *
+ * @type {import('@commitlint/types').UserConfig}
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] };
