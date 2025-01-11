67c93dd2d734fe703abb774ebabd15e38e21dd95
Nozomi Ishii <nozomiishii.jp@gmail.com>
2025-01-11 15:29:12 +0800

    fix: update prettier CLI commands for improved functionality

--- packages/prettier-config/bin/cli.sh
+++ packages/prettier-config/bin/cli.sh
@@ -13,9 +13,9 @@ pnpm add -D @nozomiishii/prettier-config
 
 echo -e "Adding npm scripts..."
 npm pkg set type="module"
-npm pkg set scripts.format="pnpm prettier --check"
-npm pkg set scripts.format:fix="pnpm prettier --write"
-npm pkg set scripts.prettier="prettier . --ignore-unknown"
+npm pkg set scripts.format="pnpm prettier . --check"
+npm pkg set scripts.format:fix="pnpm prettier . --write"
+npm pkg set scripts.prettier="prettier --ignore-unknown --ignore-path ./.gitignore"
 
 echo -e "Creating prettier.config.js"
 find . -type f -name '.prettierrc*' -delete
