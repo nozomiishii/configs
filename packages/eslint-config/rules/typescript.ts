465aeae461f81ca44ac79cc731de23c331eff2e7
Nozomi Ishii <nozomiishii.jp@gmail.com>
2025-07-02 00:28:39 +0700

    fix: improve typescript eslint configuration

--- packages/eslint-config/rules/typescript.ts
+++ packages/eslint-config/rules/typescript.ts
@@ -10,37 +10,53 @@ import { name } from '../utils/name';
  * @see https://typescript-eslint.io/rules
  */
 export function typescript() {
-  return tseslint.config(tseslint.configs.strictTypeChecked, tseslint.configs.stylisticTypeChecked, {
-    files: ['**/*.{ts,tsx}'],
-    name: name('typescript'),
-    rules: {
-      /**
-       * interfaceではなくtypeを使う
-       *
-       * @see https://typescript-eslint.io/rules/consistent-type-definitions
-       */
-      '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+  return tseslint.config(
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+      files: ['**/*.{ts,tsx}'],
+      name: name('typescript'),
+      rules: {
+        /**
+         * interfaceではなくtypeを使う
+         *
+         * @see https://typescript-eslint.io/rules/consistent-type-definitions
+         */
+        '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
 
-      // Method shorthand syntaxではなくObject property syntaxで関数の型定義する
-      // https://www.totaltypescript.com/method-shorthand-syntax-considered-harmful
-      '@typescript-eslint/method-signature-style': ['error', 'property'],
+        // Method shorthand syntaxではなくObject property syntaxで関数の型定義する
+        // https://www.totaltypescript.com/method-shorthand-syntax-considered-harmful
+        '@typescript-eslint/method-signature-style': ['error', 'property'],
 
-      // Promiseをちゃんと処理する。VoidやIIFEは無視してよい。
-      '@typescript-eslint/no-floating-promises': ['error', { ignoreIIFE: true, ignoreVoid: true }],
+        // Promiseをちゃんと処理する。VoidやIIFEは無視してよい。
+        '@typescript-eslint/no-floating-promises': ['error', { ignoreIIFE: true, ignoreVoid: true }],
 
-      /**
-       * 使ってない引数はアンダースコア始まりにする
-       *
-       * @see https://typescript-eslint.io/rules/no-unused-vars
-       */
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-        },
-      ],
+        /**
+         * 使ってない引数はアンダースコア始まりにする
+         *
+         * @see https://typescript-eslint.io/rules/no-unused-vars
+         */
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_',
+            destructuredArrayIgnorePattern: '^_',
+          },
+        ],
+      },
     },
-  }) as Linter.Config[];
+    {
+      files: ['**/*.d.ts'],
+      name: name('typescript/d.ts'),
+      rules: {
+        /**
+         * typeではなくinterfaceを使う
+         *
+         * @see https://typescript-eslint.io/rules/consistent-type-definitions
+         */
+        '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
+      },
+    },
+  ) as Linter.Config[];
 }
