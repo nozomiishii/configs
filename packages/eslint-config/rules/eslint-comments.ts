ff9bcca540ed4942f21dedec64bbead25de0a43b
Nozomi Ishii <nozomiishii.jp@gmail.com>
2026-01-19 15:50:42 +0700

    fix: update @eslint-community/eslint-plugin-eslint-comments to v4.6.0

--- packages/eslint-config/rules/eslint-comments.ts
+++ packages/eslint-config/rules/eslint-comments.ts
@@ -1,21 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import type { Linter } from 'eslint';
-// @ts-expect-error missing types
 import eslintPluginEslintComments from '@eslint-community/eslint-plugin-eslint-comments/configs';
 import { defineConfig } from 'eslint/config';
 import { name } from '../utils/name';
 
-const configs = eslintPluginEslintComments.recommended as unknown as Linter.Config;
+const configs = eslintPluginEslintComments.recommended;
 
 /**
  * @returns eslint-plugin-eslint-comments
  * {@link https://github.com/eslint-community/eslint-plugin-eslint-comments}
- *
- * d.tsなくって死んでるのつらい
- * https://github.com/eslint-community/eslint-plugin-eslint-comments/issues/214
- *
- * このルールも入れたい
- * https://github.com/eslint-community/eslint-plugin-eslint-comments/issues/190
  */
 export function eslintComments() {
   return defineConfig([
@@ -24,8 +15,6 @@ export function eslintComments() {
       name: name('eslint-comments'),
       rules: {
         ...configs.rules,
-        // 'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }]だと'use client'に対応できないから下の方がいいかも
-        // 'eslint-comments/disable-enable-pair': 'off',
         '@eslint-community/eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
       },
     },
