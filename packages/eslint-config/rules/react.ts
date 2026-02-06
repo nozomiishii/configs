2a242594c5448c1aa511dafa44b28cc4e5a30b4f
Nozomi Ishii <nozomiishii.jp@gmail.com>
2026-02-06 19:09:17 +0900

    fix: simplify react ESLint configuration by directly using recommended settings

--- packages/eslint-config/rules/react.ts
+++ packages/eslint-config/rules/react.ts
@@ -1,9 +1,6 @@
 import eslintPluginReact from '@eslint-react/eslint-plugin';
-import { type Linter } from 'eslint';
 import { defineConfig } from 'eslint/config';
 import { name } from '../utils/name';
-
-const configs = eslintPluginReact.configs['recommended-type-checked'] as unknown as Linter.Config;
 
 /**
  * @returns `@eslint-react/eslint-plugin`
@@ -14,11 +11,8 @@ export function react() {
   return defineConfig([
     {
       // https://github.com/Rel1cx/eslint-react/blob/main/packages/plugins/eslint-plugin/src/configs/recommended-type-checked.ts
-      ...configs,
+      ...eslintPluginReact.configs['recommended-type-checked'],
       name: name('react/recommended-type-checked'),
-      rules: {
-        ...configs.rules,
-      },
     },
   ]);
 }
