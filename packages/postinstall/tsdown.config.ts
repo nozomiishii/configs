92f6048f833d00b869dc5e89c12db9f94420c6d7
Nozomi Ishii <nozomiishii.jp@gmail.com>
2026-03-29 14:05:29 +0900

    chore: update dependency typescript to v6 and migrate tsup to tsdown (#1997)

---
+++ packages/postinstall/tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  platform: 'node',
+  outExtensions: () => ({ js: '.js', dts: '.d.ts' }),
+});
