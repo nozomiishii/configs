de283bf96117f04270da1878826d3d76c3bd748c
Nozomi Ishii <nozomiishii.jp@gmail.com>
2026-05-03 01:54:26 +0900

    chore(commitlint-config): remove obsolete src/starter.ts after move to package root

--- packages/commitlint-config/src/starter.ts
+++
@@ -1,6 +0,0 @@
-// 利用側プロジェクトの commitlint.config.ts に書き出されるテンプレート。
-// tsdown により dist/starter.js にビルドされ、init.ts からは re-export として読み込まれる。
-export default `import config from "@nozomiishii/commitlint-config";
-
-export default config;
-`;
