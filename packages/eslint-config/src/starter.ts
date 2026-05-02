7983544d657fdd8432c6a19cfa996f9f90a36924
Nozomi Ishii <nozomiishii.jp@gmail.com>
2026-05-03 02:13:48 +0900

    chore(eslint-config): remove obsolete src/starter.ts after move to package root

--- packages/eslint-config/src/starter.ts
+++
@@ -1,3 +0,0 @@
-// 利用側プロジェクトの eslint.config.ts に書き出されるテンプレート。
-// tsdown により dist/starter.js にビルドされ、init.ts からは re-export として読み込まれる。
-export default `export { default } from "@nozomiishii/eslint-config";\n`;
