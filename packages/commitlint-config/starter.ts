a407c066537aab507cdc81e4f15f25cea643a881
Nozomi Ishii <nozomiishii.jp@gmail.com>
2026-05-03 01:59:52 +0900

    chore(commitlint-config): replace starter.ts comment with upstream docs URLs

--- packages/commitlint-config/starter.ts
+++ packages/commitlint-config/starter.ts
@@ -1,12 +1,4 @@
-// 利用側プロジェクトの commitlint.config.ts に書き出される実体ファイル。
-// init bin はこのファイルを fs.readFileSync で読み取り、テキストのまま出力する。
-//
-// このファイル自体が利用側にコピーされる「普通の TypeScript」 として完結している必要が
-// あるため、template literal や export wrapper でラップしない。
-//
-// commitlint cli は config を `extends` フィールドで階層 merge する設計のため、
-// shared config を直接 default export するのではなく、`extends` 経由で参照する形を
-// 採る。`import config from "..."; export default config;` のような re-export では
-// commitlint 側の rule / plugins merge が期待どおりに効かないことがある。
+// @see https://commitlint.js.org/
+// @see https://github.com/nozomiishii/configs/tree/main/packages/commitlint-config
 
 export default { extends: ["@nozomiishii/commitlint-config"] };
