aae4e3e475dd1ec095f228242155b1ffb45a1617
Nozomi Ishii <nozomiishii.jp@gmail.com>
2026-09-19 23:52:56 +0900

    fix: re-enable no-nested-ternary overridden by unicorn recommended (#2888)

--- packages/eslint-config/rules/unicorn.ts
+++ packages/eslint-config/rules/unicorn.ts
@@ -22,6 +22,14 @@ export function unicorn() {
         ...eslintPluginUnicorn.configs.recommended.rules,
 
         /**
+         * 入れ子の三項演算子は unicorn/no-nested-ternary ではなく ESLint core の no-nested-ternary で禁止する。
+         *
+         * @see https://eslint.org/docs/latest/rules/no-nested-ternary
+         * @see https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/no-nested-ternary.md
+         */
+        "no-nested-ternary": "error",
+
+        /**
          * 略語の制限。やるなら明示的にreplacementを記載していく
          *
          * @see https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/name-replacements.md
