cf088330be8335ce8b869a5d928e30e849e18fbf
Nozomi Ishii <nozomiishii.jp@gmail.com>
2026-09-23 04:18:17 +0900

    fix: keep type-checked eslint-react rules off js files (#2922)

--- packages/eslint-config/rules/react.ts
+++ packages/eslint-config/rules/react.ts
@@ -12,7 +12,21 @@ export function react() {
     {
       // https://github.com/Rel1cx/eslint-react/blob/main/packages/plugins/eslint-plugin/src/configs/recommended-type-checked.ts
       ...eslintPluginReact.configs["recommended-type-checked"],
+      files: ["**/*.{ts,cts,mts,tsx}"],
       name: name("react/recommended-type-checked"),
+    },
+
+    /**
+     * .js系はtypescript/disable-type-checked-jsでprojectServiceを切っているため、
+     * 型を見るruleを含まないrecommendedを当てる。
+     *
+     * @see https://github.com/Rel1cx/eslint-react/blob/main/packages/plugins/eslint-plugin/src/configs/recommended.ts
+     * @see https://typescript-eslint.io/troubleshooting/typed-linting/#how-do-i-disable-type-checked-linting-for-a-file
+     */
+    {
+      ...eslintPluginReact.configs.recommended,
+      files: ["**/*.{js,cjs,mjs,jsx}"],
+      name: name("react/recommended-js"),
     },
   ]);
 }
